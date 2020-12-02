49bf8a7f8d2fd997596a1d4b6b0218b6ce58f001
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2020-12-02 18:31:20 +0100

    Format PasswordPolicy

--- src/main/kotlin/com/bbrakenhoff/adventofcode/day02/PasswordPolicy.kt
+++ src/main/kotlin/com/bbrakenhoff/adventofcode/day02/PasswordPolicy.kt
@@ -1,6 +1,6 @@
 package com.bbrakenhoff.adventofcode.day02
 
-abstract class PasswordPolicy(val requiredChar:Char, val digitLeft:Int, val digitRight:Int,val password: String) {
+abstract class PasswordPolicy(val requiredChar: Char, val digitLeft: Int, val digitRight: Int, val password: String) {
 
     abstract fun isPasswordValid(): Boolean
 }
