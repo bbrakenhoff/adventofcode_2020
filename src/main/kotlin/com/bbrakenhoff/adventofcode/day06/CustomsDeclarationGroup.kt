94d69d598116e4ce57e13ada1e4f7cd3482068f8
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2020-12-06 14:40:05 +0100

    Remove duplicate code in CustomsDeclarationGroup

--- src/main/kotlin/com/bbrakenhoff/adventofcode/day06/CustomsDeclarationGroup.kt
+++ src/main/kotlin/com/bbrakenhoff/adventofcode/day06/CustomsDeclarationGroup.kt
@@ -2,18 +2,16 @@ package com.bbrakenhoff.adventofcode.day06
 
 class CustomsDeclarationGroup(private val questionsYesAnswered: List<String>) {
 
+    private val allQuestionsYesAnswered = questionsYesAnswered.joinToString("")
+
     fun countAllYesAnswers(): Int {
         val questionSet: MutableSet<Char> = mutableSetOf()
-
-        questionsYesAnswered.joinToString("").map {
-            questionSet.add(it)
-        }
-
+        allQuestionsYesAnswered.map { questionSet.add(it) }
         return questionSet.size
     }
 
     fun countUnanimouslyYesAnswers(): Int {
-        val groupedQuestions: Map<Char, List<Char>> = questionsYesAnswered.joinToString("").groupBy { it }
+        val groupedQuestions: Map<Char, List<Char>> = allQuestionsYesAnswered.groupBy { it }
         val yesCountPerQuestion: Map<Char, Int> = groupedQuestions.mapValues { it.value.size }
         val unanimousYesAnswers: Map<Char, Int> = yesCountPerQuestion.filter { it.value == questionsYesAnswered.size }
         return unanimousYesAnswers.size
