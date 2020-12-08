8e8fc5619f4d2cebf1c7eead91fd492a7c48756b
bbrakenhoff <bbrakenhoff@gmail>
2020-12-08 09:01:53 +0100

    Template code and input day 08

---
+++ src/main/kotlin/com/bbrakenhoff/adventofcode/day08/Day08.kt
@@ -0,0 +1,15 @@
+package com.bbrakenhoff.adventofcode.day08
+
+import com.bbrakenhoff.adventofcode.Day
+
+class Day08 : Day {
+    override val enabledFocusedPrint: Boolean = true
+
+    override fun partOne(): String {
+        return ""
+    }
+
+    override fun partTwo(): String {
+        return ""
+    }
+}
---
+++ src/test/kotlin/com/bbrakenhoff/adventofcode/day08/Day08Test.kt
@@ -0,0 +1,29 @@
+package com.bbrakenhoff.adventofcode.day08
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+
+    private lateinit var day08: Day08
+
+    @BeforeEach
+    fun beforeEach() {
+        day08 = Day08()
+    }
+
+    @Test
+    fun `solution part one`() {
+        val expectedResult: String = ""
+        val result: String = day08.partOne()
+        result shouldBe expectedResult
+    }
+
+    @Test
+    fun `solution part two`() {
+        val expectedResult: String = ""
+        val result: String = day08.partTwo()
+        result shouldBe expectedResult
+    }
+}
