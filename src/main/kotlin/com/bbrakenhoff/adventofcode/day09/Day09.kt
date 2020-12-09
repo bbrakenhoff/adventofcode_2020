baf6d468bd3d52c89d15898aef0520a89d6f5bd9
bbrakenhoff <bbrakenhoff@gmail>
2020-12-09 11:28:04 +0100

    Template code for day 09

---
+++ src/main/kotlin/com/bbrakenhoff/adventofcode/day09/Day09.kt
@@ -0,0 +1,16 @@
+package com.bbrakenhoff.adventofcode.day09
+
+import com.bbrakenhoff.adventofcode.Day
+
+class Day09 : Day {
+
+    override val enabledFocusedPrint: Boolean = true
+
+    override fun partOne(): String {
+        return ""
+    }
+
+    override fun partTwo(): String {
+        return ""
+    }
+}
---
+++ src/test/kotlin/com/bbrakenhoff/adventofcode/day09/Day09Test.kt
@@ -0,0 +1,25 @@
+package com.bbrakenhoff.adventofcode.day09
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class Day09Test {
+
+    private lateinit var day09: Day09
+
+    @BeforeEach
+    fun beforeEach() {
+        day09 = Day09()
+    }
+
+    @Test
+    fun `solution part one`() {
+        day09.partOne() shouldBe ""
+    }
+
+    @Test
+    fun `solution part two`() {
+        day09.partTwo() shouldBe ""
+    }
+}
