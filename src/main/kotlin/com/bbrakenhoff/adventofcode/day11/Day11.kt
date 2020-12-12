338445daec49cb9d5d6f926dede08427c792afe7
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2020-12-12 01:14:47 +0100

    Solution day 11 read puzzle input again for part two

--- src/main/kotlin/com/bbrakenhoff/adventofcode/day11/Day11.kt
+++ src/main/kotlin/com/bbrakenhoff/adventofcode/day11/Day11.kt
@@ -4,12 +4,11 @@ import com.bbrakenhoff.adventofcode.Day
 import com.bbrakenhoff.adventofcode.PuzzleInputReader
 
 class Day11 : Day {
+
     override val enabledFocusedPrint: Boolean = true
 
-    private val initialSeatLayout: MutableList<String> = PuzzleInputReader.read(11).toMutableList()
-
     override fun partOne(): String {
-        val seatLayout: SeatLayout = SeatLayout(initialSeatLayout)
+        val seatLayout: SeatLayout = SeatLayout( PuzzleInputReader.read(11).toMutableList())
         val finalSeeatLayout = seatLayout.findFinalSeatLayout()
         val mapped = seatLayout.mapToSeats(finalSeeatLayout)
         val occupied = mapped.sumBy { it.count { seat -> seat.isOccupied } }
@@ -17,7 +16,7 @@ class Day11 : Day {
     }
 
     override fun partTwo(): String {
-        val seatLayout: SeatLayout = SeatLayout(initialSeatLayout)
+        val seatLayout: SeatLayout = SeatLayout( PuzzleInputReader.read(11).toMutableList())
         val finalSeeatLayout = seatLayout.findFinalSeatLayout2()
         val mapped = seatLayout.mapToSeats(finalSeeatLayout)
         val occupied = mapped.sumBy { it.count { seat -> seat.isOccupied } }
